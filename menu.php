9dd110363d449b8c7bfa4615ad38360ce24d04dd
rihabdk <82368207+rihabdk@users.noreply.github.com>
2021-08-22 22:55:39 +0100

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="../index.php" class="navbar-brand"> Gestion du staff médical </a>
+</div>
+    <ul class="nav navbar-nav">
+        <li> <a href="staff.php"> staff médical </a>  </li>
+        <li> <a href="gestionnaires.php"> gestionnaires d'un hopital </a>  </li>
+        
+</ul>
+</div>
+</nav>
